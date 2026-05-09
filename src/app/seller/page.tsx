28ff02dd79c6ff42c1dab280b1e847b2bd89c565
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2026-05-10 03:18:01 +0800

    working mui with icons

---
+++ src/app/seller/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import AbcIcon from '@mui/icons-material/Abc';
+
+const page = () => {
+  return <div><Button variant="contained">Hello world<AbcIcon /></Button></div>;
+};
+
+export default page;
